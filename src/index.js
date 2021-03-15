6c90b4cbd3f74e78b5617bd33890ed9a64326080
Andrew <ankara7979@gmail,com>
2021-03-15 16:40:13 +0200

    feat: add solution js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let acc = [];
+    let array = str.split("");
+    let count = 0;
+
+    while(count < array.length){
+        acc.push(array[count]);
+        for (let i = 0; i < bracketsConfig.length; i++){
+            if (acc[acc.length-2] === bracketsConfig[i][0] && acc[acc.length-1] === bracketsConfig[i][1]){
+                acc.pop();
+                acc.pop();
+            }
+        }
+        count++;
+    }
+    return !acc.length;
 }
